d3abccb6895086a8bfad26f883fa5080e675ed3f
Krish Vijay Ramani <136609165+KrishRamani2@users.noreply.github.com>
2024-08-20 00:27:46 +0530

    Delete backend directory

--- backend/database/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose"); 
-mongoose.
-connect(process.env.URL).then(()=>{
-    console.log("Database Connected successfully...");
-}).catch((e)=>{
-    console.log("Error...")
-});
-
